58fb82ef267cb9e5049858909ab2211664e97afb
Julian Rios <julirg58@gmail.com>
2020-11-04 18:12:05 -0500

    todos los puntos completos

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-// Ejercicio no.4 manejo de objetos y arreglos
-//Nota: 
-//1. En cada una de las funciones deberás de usar el parametro feed para realizar las operaciones
-//2. A partir de la función no 3 se deberá de regresar como resultado el arreglo de tweets con todas sus propiedades y ////// valores actualizados acorde a las operaciones de cada uno
+// Ejercicio no.4 manejo de objetos y arreglos Nota:
+// 1. En cada una de las funciones deberás de usar el parametro feed para
+// realizar las operaciones
+// 2. A partir de la función no 3 se deberá de regresar como resultado el
+// arreglo de tweets con todas sus propiedades y ////// valores actualizados
+// acorde a las operaciones de cada uno
 const {feed} = require('./source');
 /*
 1. Obtener el numero total de Tweets
 Input: feed -> [{propiedades...}, {propiedades...}]
 Output: 2
 */
-const countTweets = (feed) => {return feed.length};
-console.log(countTweets(feed));
+const countTweets = (feed) => {
+    return feed.length
+};
+//console.log(countTweets(feed));
 /*
 2. Obtener un arreglo con todos los perfiles de usuarios que están en el arreglo de tweets
 Input: feed -> [{profile: "AJ + Español"},{ profile: "Elon Musk" }]
 Output: ["AJ + Español", "Elon Musk"]
 */
 
-const getProfiles = (feed) => {}
+const getProfiles = (feed) => {
+    const profiles = feed.map(element => element.profile)
+    return profiles;
+}
 
 /*
 3. Crear una función para obtener el arreglo de tweets con todas sus propiedades pero se tendrá que agregar un '@' al inicio del valor para la propiedad username
@@ -24,11 +31,18 @@ Input:  feed -> [{username: "ajplusespanol", ...},{ username: "elonmusk", ... }]
 Output: [{username: "@ajplusespanol"}, {username: "@elonmusk"}]
 */
 
-const updateUsername = (feed) => {}
+const updateUsername = (feed) => {
+    const newFeed = JSON.parse(JSON.stringify(feed))
+    const coppy = newFeed.map(element => {
+        element.username = '@' + element.username;
+        return element
+    })
+    return coppy
+}
 
 /*
-4. Crear una función para actualizar el no de interacciones de cada tweet 
-para que en los casos donde haya más de 1000 (likes, comentarios, retweets) 
+4. Crear una función para actualizar el no de interacciones de cada tweet
+para que en los casos donde haya más de 1000 (likes, comentarios, retweets)
 se muestre el noInteracción/1000 y la palabra mil
 Input: feed -> [
     {
@@ -38,7 +52,7 @@ Input: feed -> [
             likes: 84000
         }
     },
-    { 
+    {
         interaction: {
             comments: 17600,
             retweets: 1300,
@@ -53,7 +67,7 @@ Output: arregloTweets -> [
             likes: "84 mil"
         }
     },
-    { 
+    {
         interaction: {
             comments: "17.6 mil",
             retweets: "1.3 mil",
@@ -63,52 +77,29 @@ Output: arregloTweets -> [
 */
 
 const updateInteraction = (feed) => {
-    // const newFeed=Array.from(feed)
-    // const coppy= newFeed.filter(element=>{
-    // if(element.interaction.comments>=1000){
-    //     element.interaction.comments=element.interaction.comments/1000 + ' mil'
-    // }
-    // if(element.interaction.retweets>=1000){
-    //     element.interaction.retweets=element.interaction.retweets/1000 + ' mil'
-    // }
-    // if(element.interaction.likes>=1000){
-    //     element.interaction.likes=element.interaction.likes/1000 + ' mil'
-    // }
-    
-    //     return element;
-    // //    element.interaction.comments>=1000? element.interaction.comments=element.interaction.comments/1000 +'mil':element.interaction.comments=element.interaction.comments
-    // })
-    
-//     return (newFeed);
-//const newFeed = JSON.parse(JSON.stringify(feed))
-const newFeed=[...feed]
-// const =Array.from(feed)
-const coppy = newFeed.map(element=>{
-    
-    if(element.interaction.comments>=1000){
-       element.interaction.comments=element.interaction.comments/1000 + ' mil'
-    }
-    if(element.interaction.retweets>=1000){
-        element.interaction.retweets=element.interaction.retweets/1000 + ' mil'
-    }
-    if(element.interaction.likes>=1000){
-                element.interaction.likes=element.interaction.likes/1000 + ' mil'
-    }
-    return element;
-})
-return (coppy);
+    const newFeed = JSON.parse(JSON.stringify(feed))
+    const copy = newFeed.map(element => {
+        let {comments, likes, retweets} = element.interaction;
+        element.interaction.comments = comments >= 1000
+            ? comments / 1000 + ' mil'
+            : comments;
+        element.interaction.likes = likes >= 1000
+            ? likes / 1000 + ' mil'
+            : likes;
+        element.interaction.retweets = retweets >= 1000
+            ? retweets / 1000 + ' mil'
+            : retweets;
+        return element;
+    })
+    return (copy);
 }
-// console.log(feed);
-// console.log('arreglo modificado');
-console.log(updateInteraction(feed));
-console.log('arreglo original')
-console.log(feed);
+
 /*
 5. Función para obtener todos los tweets que tengan más de "n" comentarios y mostrarlos de la misma forma que el punto no 4
-Input: 
+Input:
 
     feed -> [
-        { 
+        {
             interaction: {
                 comments: 5,
                 retweets: 66,
@@ -122,7 +113,7 @@ Input:
                 likes: 84000
             }
         },
-        { 
+        {
             interaction: {
                 comments: 17600,
                 retweets: 1300,
@@ -142,7 +133,7 @@ Output: feed -> [
             likes: "84 mil"
         }
     },
-    { 
+    {
         interaction: {
             comments: "17.6 mil",
             retweets: "1.3 mil",
@@ -151,15 +142,29 @@ Output: feed -> [
     }]
 */
 const getTweetsGreaterThan = (feed, noComments) => {
+    const newFeed=feed.filter(element=>element.interaction.comments>noComments);
+    return newFeed;
+        // const newFeed = JSON.parse(JSON.stringify(feed))
+        // const copy = newFeed.filter(element => {
+        //     if(element.interaction.comments > noComments){
+        //         element.interaction.comments>=1000?element.interaction.comments=element.interaction.comments/1000 +' mil':element.interaction.comments
+        //         element.interaction.retweets>=1000?element.interaction.retweets=element.interaction.retweets/1000 +' mil':element.interaction.retweets
+        //         element.interaction.likes>=1000?element.interaction.likes=element.interaction.likes/1000 +' mil':element.interaction.likes
+
+        //         return element
+        //     }
+            
+        // });
+        // return (copy);
 
 }
-
+console.log(getTweetsGreaterThan(feed,2000))
 /*
 6. Función para obtener todos los tweets que tengan un numero menor o igual de "n" comentarios y mostrarlos de la misma forma que el punto no 4
-Input: 
+Input:
 
     feed -> [
-        { 
+        {
             interaction: {
                 comments: 5,
                 retweets: 66,
@@ -173,7 +178,7 @@ Input:
                 likes: 84000
             }
         },
-        { 
+        {
             interaction: {
                 comments: 17600,
                 retweets: 1300,
@@ -193,7 +198,7 @@ Output: arregloTweets -> [
             likes: "84 mil"
         }
     },
-    { 
+    {
         interaction: {
             comments: "17.6 mil",
             retweets: "1.3 mil",
@@ -202,16 +207,29 @@ Output: arregloTweets -> [
     }]
 */
 const getTweetsLessOrEqualsThan = (feed, noComments) => {
+    const newArray = feed.filter(element => element.interaction.comments <= noComments);
+    return (newArray);
+    //   const newFeed = JSON.parse(JSON.stringify(feed))
+    //     const copy = newFeed.filter(element => {
+    //         if(element.interaction.comments <= noComments){
+    //             element.interaction.comments>=1000?element.interaction.comments=element.interaction.comments/1000 +' mil':element.interaction.comments
+    //             element.interaction.retweets>=1000?element.interaction.retweets=element.interaction.retweets/1000 +' mil':element.interaction.retweets
+    //             element.interaction.likes>=1000?element.interaction.likes=element.interaction.likes/1000 +' mil':element.interaction.likes
 
+    //             return element
+    //         }
+            
+    //     });
+    //     return (copy);
 }
 
 /*
     7. Función para obtener todos los tweets que tengan un numero mayor de "n" [interacción] y mostrarlos de la misma forma que el punto no 4
-    
+
     Input =========
 
     feed -> [
-        { 
+        {
             interaction: {
                 comments: 5,
                 retweets: 66,
@@ -225,7 +243,7 @@ const getTweetsLessOrEqualsThan = (feed, noComments) => {
                 likes: 84000
             }
         },
-        { 
+        {
             interaction: {
                 comments: 17600,
                 retweets: 1300,
@@ -238,7 +256,7 @@ const getTweetsLessOrEqualsThan = (feed, noComments) => {
 
     Output ============
     [
-        { 
+        {
             interaction: {
                 comments: 17600,
                 retweets: 1300,
@@ -250,7 +268,17 @@ const getTweetsLessOrEqualsThan = (feed, noComments) => {
 */
 const getTweetsGreaterThanInteraction = (feed, interaction, quantity) => {
     // const newFeed = JSON.parse(JSON.stringify(feed))
-    const newFeed= feed.filter(element=>element.interaction[interaction]>=quantity)    
+    const newFeed = feed.filter(element => element.interaction[interaction] >= quantity)
     return (newFeed);
 }
-console.log(getTweetsGreaterThanInteraction(feed,'comments',6700))
+// console.log(getTweetsGreaterThanInteraction(feed,'comments',6700))
+module.exports = {
+    countTweets,
+    getProfiles,
+    updateUsername,
+    updateInteraction,
+    getTweetsGreaterThan,
+    getTweetsLessOrEqualsThan,
+    getTweetsGreaterThanInteraction
+
+}
